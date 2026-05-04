d66761348e23a3c16ada7f2ebfbd49af892d812f
Hasan Mavlonov <hmavlanov79@gmail.com>
2026-05-05 02:31:30 +0800

    Fix Django project structure and static URL wiring

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
     path('', include('landing.urls')),
     path('admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
