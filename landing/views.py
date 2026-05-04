dbc1c6938d3ecc590ae3d65cc8444fce37e68159
Hasan Mavlonov <hmavlanov79@gmail.com>
2026-05-05 02:03:28 +0800

    Fix template static asset resolution and add axis page routes

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "landing/home.html")
+
+
+def axis_home(request):
+    return render(request, "axis/index.html")
+
+
+def axis_landing(request):
+    return render(request, "axis/landing-page.html")
+
+
+def axis_observer(request):
+    return render(request, "axis/the-observer.html")
